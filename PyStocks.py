fa83bccce01f804ef57d4414a10cc76cf6ae241c
Andrew Rowell <andrewjrowell@gmail.com>
2020-04-27 11:29:07 -0500

    Add endpoint to render daily price chart.

--- PyStocks.py
+++ PyStocks.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask import request
 from flask import Response
 from Creds import Creds
+import matplotlib.pyplot as plt
 from alpha_vantage.timeseries import TimeSeries
+import mpld3
 
 app = Flask(__name__)
 ts = TimeSeries(key=Creds().getApiKey(),output_format='pandas')
@@ -11,7 +13,7 @@ ts = TimeSeries(key=Creds().getApiKey(),output_format='pandas')
 def home():
     return "Home Page"
 
-@app.route('/currentprice')
+@app.route('/current_price')
 def getCurrentPrice():
     symbol = request.args.get('symbol')
     if symbol == None:
@@ -19,6 +21,19 @@ def getCurrentPrice():
     try:
         price = ts.get_intraday(symbol=symbol)[0].iloc[-1]['4. close']
         return str(price)
+    except:
+        return Response("An error occurred", status=500)
+
+@app.route('/daily_chart')
+def getCurrentChart():
+    symbol = request.args.get('symbol')
+    if symbol == None:
+        return Response("No symbol given!", status=400)
+    try:
+        fig = plt.figure()
+        plt.plot(ts.get_daily_adjusted(symbol=symbol)[0]['5. adjusted close'])
+        chart_html = mpld3.fig_to_html(fig)
+        return chart_html
     except:
         return Response("An error occurred", status=500)
 
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # PyStocks
 Flask-Based Stock Data Viewer
 
+## How to Run
+Set up the anaconda environment from pystocks.yml, then run `python PyStocks.py`
+
+## API
+### Home Page
+`https://localhost:5000/` returns an empty page. I might add some kind of single page GUI here, someday.
+### Current Price
+`https://localhost:5000/current_price?symbol=[SYMBOL]` returns the recent price of a given stock.
+### Daily Chart
+`https://localhost:5000/daily_chart?symbol=[SYMBOL]` returns a price chart for the specified stock.
+
 ## Making Changes
 ### Creds.py
 Since this file may contain login info, I've configured git to [not add](https://stackoverflow.com/a/39776107) its changes.
